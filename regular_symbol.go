a6a61e2fabb5ddf11c609ca16625397adb8726ab
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-04-25 12:58:46 -0300

    feat: implement regular symbol building for QR codes
    This commit introduces the functionality to build regular QR code symbols based on the provided data, version, and mask.
    
    - Implements the `buildRegularSymbol` function, which takes a QR code version, mask, data bitset, and a flag for including a quiet zone as input.
    - The function constructs the QR code symbol by adding finder patterns, alignment patterns, timing patterns, format information, version information, and data bits.
    - Adds helper functions for placing the finder patterns, alignment patterns, and timing patterns.
    - Implements the data encoding process, including applying the mask pattern and placing the data bits in the symbol.
    - Adds constants for finder pattern, alignment pattern, and their sizes.
    - Adds a test case for building regular symbols in `regular_symbol_test.go`.

---
+++ regular_symbol.go
@@ -0,0 +1,294 @@
+package qrcode
+
+import (
+	bitset "github.com/i9si-sistemas/bitset"
+)
+
+type regularSymbol struct {
+	version qrCodeVersion
+	mask    int
+
+	data *bitset.Bitset
+
+	symbol *symbol
+	size   int
+}
+
+var (
+	alignmentPatternCenter = [][]int{
+		{}, 
+		{}, 
+		{6, 18},
+		{6, 22},
+		{6, 26},
+		{6, 30},
+		{6, 34},
+		{6, 22, 38},
+		{6, 24, 42},
+		{6, 26, 46},
+		{6, 28, 50},
+		{6, 30, 54},
+		{6, 32, 58},
+		{6, 34, 62},
+		{6, 26, 46, 66},
+		{6, 26, 48, 70},
+		{6, 26, 50, 74},
+		{6, 30, 54, 78},
+		{6, 30, 56, 82},
+		{6, 30, 58, 86},
+		{6, 34, 62, 90},
+		{6, 28, 50, 72, 94},
+		{6, 26, 50, 74, 98},
+		{6, 30, 54, 78, 102},
+		{6, 28, 54, 80, 106},
+		{6, 32, 58, 84, 110},
+		{6, 30, 58, 86, 114},
+		{6, 34, 62, 90, 118},
+		{6, 26, 50, 74, 98, 122},
+		{6, 30, 54, 78, 102, 126},
+		{6, 26, 52, 78, 104, 130},
+		{6, 30, 56, 82, 108, 134},
+		{6, 34, 60, 86, 112, 138},
+		{6, 30, 58, 86, 114, 142},
+		{6, 34, 62, 90, 118, 146},
+		{6, 30, 54, 78, 102, 126, 150},
+		{6, 24, 50, 76, 102, 128, 154},
+		{6, 28, 54, 80, 106, 132, 158},
+		{6, 32, 58, 84, 110, 136, 162},
+		{6, 26, 54, 82, 110, 138, 166},
+		{6, 30, 58, 86, 114, 142, 170},
+	}
+
+	finderPattern = [][]bool{
+		{false, false, false, false, false, false, false},
+		{false, true, true, true, true, true, false},
+		{false, true, false, false, false, true, false},
+		{false, true, false, false, false, true, false},
+		{false, true, false, false, false, true, false},
+		{false, true, true, true, true, true, false},
+		{false, false, false, false, false, false, false},
+	}
+
+	finderPatternSize = 7
+
+	finderPatternHorizontalBorder = [][]bool{
+		{true, true, true, true, true, true, true, true},
+	}
+
+	finderPatternVerticalBorder = [][]bool{
+		{true},
+		{true},
+		{true},
+		{true},
+		{true},
+		{true},
+		{true},
+		{true},
+	}
+
+	alignmentPattern = [][]bool{
+		{false, false, false, false, false},
+		{false, true, true, true, false},
+		{false, true, false, true, false},
+		{false, true, true, true, false},
+		{false, false, false, false, false},
+	}
+)
+
+func buildRegularSymbol(
+	version qrCodeVersion, 
+	mask int,
+	data *bitset.Bitset, 
+	includeQuietZone bool,
+) (*symbol, error) {
+	quietZoneSize := 0
+	if includeQuietZone {
+		quietZoneSize = version.quietZoneSize()
+	}
+
+	m := &regularSymbol{
+		version: version,
+		mask:    mask,
+		data:    data,
+
+		symbol: newSymbol(version.symbolSize(), quietZoneSize),
+		size:   version.symbolSize(),
+	}
+
+	m.addFinderPatterns()
+	m.addAlignmentPatterns()
+	m.addTimingPatterns()
+	m.addFormatInfo()
+	m.addVersionInfo()
+
+	ok, err := m.addData()
+	if !ok {
+		return nil, err
+	}
+
+	return m.symbol, nil
+}
+
+func (m *regularSymbol) addFinderPatterns() {
+	fpSize := finderPatternSize
+	fp := finderPattern
+	fpHBorder := finderPatternHorizontalBorder
+	fpVBorder := finderPatternVerticalBorder
+
+	m.symbol.set2dPattern(0, 0, fp)
+	m.symbol.set2dPattern(0, fpSize, fpHBorder)
+	m.symbol.set2dPattern(fpSize, 0, fpVBorder)
+
+	m.symbol.set2dPattern(m.size-fpSize, 0, fp)
+	m.symbol.set2dPattern(m.size-fpSize-1, fpSize, fpHBorder)
+	m.symbol.set2dPattern(m.size-fpSize-1, 0, fpVBorder)
+
+	m.symbol.set2dPattern(0, m.size-fpSize, fp)
+	m.symbol.set2dPattern(0, m.size-fpSize-1, fpHBorder)
+	m.symbol.set2dPattern(fpSize, m.size-fpSize-1, fpVBorder)
+}
+
+func (m *regularSymbol) addAlignmentPatterns() {
+	for _, x := range alignmentPatternCenter[m.version.version] {
+		for _, y := range alignmentPatternCenter[m.version.version] {
+			if !m.symbol.empty(x, y) {
+				continue
+			}
+
+			m.symbol.set2dPattern(x-2, y-2, alignmentPattern)
+		}
+	}
+}
+
+func (m *regularSymbol) addTimingPatterns() {
+	value := true
+
+	for i := finderPatternSize + 1; i < m.size-finderPatternSize; i++ {
+		m.symbol.set(i, finderPatternSize-1, value)
+		m.symbol.set(finderPatternSize-1, i, value)
+
+		value = !value
+	}
+}
+
+func (m *regularSymbol) addFormatInfo() {
+	fpSize := finderPatternSize
+	l := formatInfoLengthBits - 1
+
+	f := m.version.formatInfo(m.mask)
+
+	for i := 0; i <= 7; i++ {
+		m.symbol.set(m.size-i-1, fpSize+1, f.At(l-i))
+	}
+
+	for i := 0; i <= 5; i++ {
+		m.symbol.set(fpSize+1, i, f.At(l-i))
+	}
+
+	m.symbol.set(fpSize+1, fpSize, f.At(l-6))
+	m.symbol.set(fpSize+1, fpSize+1, f.At(l-7))
+	m.symbol.set(fpSize, fpSize+1, f.At(l-8))
+
+	for i := 9; i <= 14; i++ {
+		m.symbol.set(14-i, fpSize+1, f.At(l-i))
+	}
+
+	for i := 8; i <= 14; i++ {
+		m.symbol.set(fpSize+1, m.size-fpSize+i-8, f.At(l-i))
+	}
+
+	m.symbol.set(fpSize+1, m.size-fpSize-1, true)
+}
+
+func (m *regularSymbol) addVersionInfo() {
+	fpSize := finderPatternSize
+
+	v := m.version.versionInfo()
+	l := versionInfoLengthBits - 1
+
+	if v == nil {
+		return
+	}
+
+	for i := range v.Len() {
+		m.symbol.set(i/3, m.size-fpSize-4+i%3, v.At(l-i))
+		m.symbol.set(m.size-fpSize-4+i%3, i/3, v.At(l-i))
+	}
+}
+
+type direction uint8
+
+const (
+	up direction = iota
+	down
+)
+
+func (m *regularSymbol) addData() (bool, error) {
+	xOffset := 1
+	dir := up
+
+	x := m.size - 2
+	y := m.size - 1
+
+	for i := range m.data.Len() {
+		var mask bool
+		switch m.mask {
+		case 0:
+			mask = (y+x+xOffset)%2 == 0
+		case 1:
+			mask = y%2 == 0
+		case 2:
+			mask = (x+xOffset)%3 == 0
+		case 3:
+			mask = (y+x+xOffset)%3 == 0
+		case 4:
+			mask = (y/2+(x+xOffset)/3)%2 == 0
+		case 5:
+			mask = (y*(x+xOffset))%2+(y*(x+xOffset))%3 == 0
+		case 6:
+			mask = ((y*(x+xOffset))%2+((y*(x+xOffset))%3))%2 == 0
+		case 7:
+			mask = ((y+x+xOffset)%2+((y*(x+xOffset))%3))%2 == 0
+		}
+
+		m.symbol.set(x+xOffset, y, mask != m.data.At(i))
+
+		if i == m.data.Len()-1 {
+			break
+		}
+
+		for {
+			if xOffset == 1 {
+				xOffset = 0
+			} else {
+				xOffset = 1
+
+				if dir == up {
+					if y > 0 {
+						y--
+					} else {
+						dir = down
+						x -= 2
+					}
+				} else {
+					if y < m.size-1 {
+						y++
+					} else {
+						dir = up
+						x -= 2
+					}
+				}
+			}
+
+			if x == 5 {
+				x--
+			}
+
+			if m.symbol.empty(x+xOffset, y) {
+				break
+			}
+		}
+	}
+
+	return true, nil
+}
