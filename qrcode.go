d53f569c60347437369efe5f829eb5ed08c66350
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-04-28 12:19:39 -0300

    feat: add WriteFileWithoutSize method
    This commit introduces a new method, `WriteFileWithoutSize`, to the `QRCode` struct. This method allows writing a PNG image of the QR code to a file using a default size, eliminating the need to specify the size explicitly. This provides a convenient way to generate QR code images with a predefined resolution.

--- qrcode.go
+++ qrcode.go
@@ -148,7 +148,9 @@ func WriteFile(content string, level RecoveryLevel, size int, filename string) e
 	if err != nil {
 		return err
 	}
-
+	if size == 0 {
+		return q.WriteFileWithoutSize(filename)
+	}
 	return q.WriteFile(size, filename)
 }
 
@@ -247,6 +249,17 @@ func (q *QRCode) WriteFile(size int, filename string) error {
 		return err
 	}
 
+	return os.WriteFile(filename, png, os.FileMode(0644))
+}
+
+const DefaultFileSize = 256
+
+// WriteFileWithoutSize writes a PNG image of the QRCode without size.
+func (q *QRCode) WriteFileWithoutSize(filename string) error {
+	png, err := q.PNG(DefaultFileSize)
+	if err != nil {
+		return err
+	}
 	return os.WriteFile(filename, png, os.FileMode(0644))
 }
 
